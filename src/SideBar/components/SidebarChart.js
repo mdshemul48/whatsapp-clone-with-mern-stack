9c9d1df09c0db47d84944a75639664327ee9078a
MD Shimul <mdshemul480@gmail.com>
2021-06-22 16:35:55 +0600

    adding room image name and last message

--- src/SideBar/components/SidebarChart.js
+++ src/SideBar/components/SidebarChart.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
-
+// avatar
+import { Avatar } from '@material-ui/core'
 // css
 import classes from "./SidebarChart.module.css"
 const SidebarChart = () => {
     return (
         <div className={classes.sidebar__chat}>
-
+            <Avatar />
+            <div className={classes.sidebar__chatInfo}>
+                <h2>Room Name</h2>
+                <p>This is the last message</p>
+            </div>
         </div>
     )
 }
