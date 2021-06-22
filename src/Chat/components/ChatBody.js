0811f3cf18abacb404d8f2559efbc8f6c35f1ae4
MD Shimul <mdshemul480@gmail.com>
2021-06-22 20:08:09 +0600

    saparating the chat message component

--- src/Chat/components/ChatBody.js
+++ src/Chat/components/ChatBody.js
@@ -4,33 +4,15 @@ import React from 'react'
 // COMPONENT_NAME: chat body 
 // INFO: all message(chat) will show here.. this is the main body section.
 //---
-
+import ChatMessage from './ChatMessage'
 import classes from "./ChatBody.module.css"
 const ChatBody = () => {
     return (
         <div className={classes.chat__body}>
-            <p className={classes.chat__message}>
-                <span className={classes.chat__name}>sam</span>
-                This is the message
-                <span className={classes.chat__timestamp}>
-                    {new Date().toUTCString()}
-                </span>
-            </p>
-
-            <p className={`${classes.chat__message} ${classes.chat__receiver}`}>
-                <span className={classes.chat__name}>sam</span>
-                This is the message
-                <span className={classes.chat__timestamp}>
-                    {new Date().toUTCString()}
-                </span>
-            </p>
-            <p className={classes.chat__message}>
-                <span className={classes.chat__name}>sam</span>
-                This is the message
-                <span className={classes.chat__timestamp}>
-                    {new Date().toUTCString()}
-                </span>
-            </p>
+            <ChatMessage />
+            <ChatMessage receiver />
+            <ChatMessage />
+            <ChatMessage />
         </div>
     )
 }
---
+++ src/Chat/components/ChatMessage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+//---
+// COMPONENT_NAME: chat message component
+// INFO: this will only show the message, name, time.
+//---
+
+import classes from "./ChatMessage.module.css"
+function ChatMessage(props) {
+    return (
+        <p className={`${classes.chat__message} ${props.receiver ? classes.chat__receiver : ""}`}>
+            <span className={classes.chat__name}>{props.name || "sam"}</span>
+            {props.message || "test message for now"}
+            <span className={classes.chat__timestamp}>
+                {props.date?.toUTCString() || new Date().toUTCString()}
+            </span>
+        </p>
+    )
+}
+
+export default ChatMessage
