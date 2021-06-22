2d22d1e01a21c87365e4ebc6538a5035ac9fe01f
MD Shimul <mdshemul480@gmail.com>
2021-06-22 16:32:06 +0600

    adding sidebar chat and sidebar chats component

---
+++ src/SideBar/components/SidebarChart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+// css
+import classes from "./SidebarChart.module.css"
+const SidebarChart = () => {
+    return (
+        <div className={classes.sidebar__chat}>
+
+        </div>
+    )
+}
+
+export default SidebarChart
---
+++ src/SideBar/components/SidebarChats.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// css 
+import classes from "./SidebarChats.module.css"
+const SidebarChats = () => {
+    return (
+        <div className={classes.sidebar__chats}>
+
+        </div>
+    )
+}
+
+export default SidebarChats
