40b6f26ccb57e2122229512ec30dac214f381760
MD Shimul <mdshemul480@gmail.com>
2021-06-22 17:34:23 +0600

    adding header info

--- src/Chat/components/ChatHeader.js
+++ src/Chat/components/ChatHeader.js
@@ -12,6 +12,10 @@ const ChatHeader = () => {
     return (
         <div className={classes.chat__header}>
             <Avatar />
+            <div className={classes.chat__headerInfo}>
+                <h3>Room Name</h3>
+                <p>Last seen at...</p>
+            </div>
         </div>
     )
 }
