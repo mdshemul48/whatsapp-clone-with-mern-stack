7a45e54f2d2255056cf019531d9851339268bfe6
MD Shimul <mdshemul480@gmail.com>
2021-06-22 12:37:20 +0600

    adding config info

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCO0lO15KOOwmOApoceX_K8jOc-2OnbuVY",
+    authDomain: "whatsapp-clone-480.firebaseapp.com",
+    projectId: "whatsapp-clone-480",
+    storageBucket: "whatsapp-clone-480.appspot.com",
+    messagingSenderId: "398852069980",
+    appId: "1:398852069980:web:dba5c8516180121f2aa499"
+};
