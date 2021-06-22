2a875138db95a2ff7513b4daf3fc2b89e468bcb6
MD Shimul <mdshemul480@gmail.com>
2021-06-22 19:01:57 +0600

    adding chat body

---
+++ src/Chat/components/ChatBody.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//---
+// COMPONENT_NAME: chat body 
+// INFO: all message(chat) will show here.. this is the main body section.
+//---
+
+import classes from "./ChatBody.module.css"
+const ChatBody = () => {
+    return (
+        <div className={classes.chat__body}>
+
+        </div>
+    )
+}
+
+export default ChatBody
