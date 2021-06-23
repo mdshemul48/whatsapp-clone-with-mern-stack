5ab9b5ebf7d3ad4c8fd15073392e876b7aba5641
MD Shimul <mdshemul480@gmail.com>
2021-06-23 14:43:21 +0600

    connecting db

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,18 +1,27 @@
 // importing
+import dotenv from "dotenv"
 import express from "express";
-
+import mongoose from "mongoose";
 
 
 
 // app config
+dotenv.config()
+
 const app = express()
 const port = process.env.PORT || 9000
-
-
 
 // middleware
 
 // db config
+const db_connection_url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.91aij.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+mongoose.connect(db_connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
 
 // ???
 
