b7020ee5e3a8bd8e8ce48f7e9162dc0ee1a064c7
MD Shimul <mdshemul480@gmail.com>
2021-06-23 22:15:47 +0600

    adding message sync function

--- whatsapp-backend/controllers/message.js
+++ whatsapp-backend/controllers/message.js
@@ -15,3 +15,15 @@ export const createNewMessage = async (req, res) => {
     return res.status(201).json(createdMessage)
 
 }
+
+// this will get all the messages from the db and return
+export const syncNewMessage = async (req, res) => {
+    let allMessage;
+    try {
+        allMessage = await NewMessage.find()
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send(err)
+    }
+    return res.status(200).json(allMessage)
+}
--- whatsapp-backend/routes/message.js
+++ whatsapp-backend/routes/message.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 
 // importing controllers 
-import { createNewMessage } from "../controllers/message.js";
+import { createNewMessage, syncNewMessage } from "../controllers/message.js";
+
+
+
 const router = Router()
 
+router.get("/sync", syncNewMessage)
 router.post("/new", createNewMessage)
+
 
 export default router;
