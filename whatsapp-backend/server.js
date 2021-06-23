ecdce02f608763e8bd98d143bb2ec42f279a5889
MD Shimul <mdshemul480@gmail.com>
2021-06-23 14:14:47 +0600

    creating hello world with express and node.js

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,6 +1,14 @@
 // importing
+import express from "express";
+
+
+
 
 // app config
+const app = express()
+const port = process.env.PORT || 9000
+
+
 
 // middleware
 
@@ -10,5 +18,8 @@
 
 // api routes
 
+app.get("/", (req, res) => res.status(200).send("hello world"))
+
 // listen
 
+app.listen(port, () => console.log(`api rocks on http://localhost:${port}`))
