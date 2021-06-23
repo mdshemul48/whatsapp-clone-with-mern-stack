6b782841bfa285d0a2886aa684b108fd8b6f2486
MD Shimul <mdshemul480@gmail.com>
2021-06-23 13:40:43 +0600

    adding chat footer

--- src/Chat/components/ChatFooter.js
+++ src/Chat/components/ChatFooter.js
@@ -1,9 +1,21 @@
+import { InsertEmoticon, Mic as MicIcon } from '@material-ui/icons'
 import React from 'react'
 
+//---
+// COMPONENT_NAME: chat footer
+// INFO: this will be the message box. where user will type message and send.
+//---
+
+import classes from "./ChatFooter.module.css"
 const ChatFooter = () => {
     return (
-        <div>
-            
+        <div className={classes.chat__footer}>
+            <InsertEmoticon />
+            <form>
+                <input type="text" placeholder="Type a message" />
+                <button type="submit">Send a message</button>
+            </form>
+            <MicIcon />
         </div>
     )
 }
