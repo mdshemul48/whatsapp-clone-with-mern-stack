310a7e99d26a1851313cf93e146743c6ec229f43
MD Shimul <mdshemul480@gmail.com>
2021-06-23 12:57:06 +0600

    linking footer to the chat component

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import classes from "./Chat.module.css"
 import ChatBody from './components/ChatBody'
+import ChatFooter from './components/ChatFooter'
 import ChatHeader from './components/ChatHeader'
 const Chat = () => {
     return (
         <div className={classes.chat}>
             <ChatHeader />
             <ChatBody />
-
+            <ChatFooter />
         </div>
     )
 }
---
+++ src/Chat/components/ChatFooter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatFooter = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ChatFooter
