0e7bd86607a6e48568b36e4bf5e3808edae224b3
MD Shimul <mdshemul480@gmail.com>
2021-06-24 13:53:33 +0600

    adding pusher for getting letest messages in frontend without refresh

--- whatsapp-frontend/src/Chat/components/ChatBody.js
+++ whatsapp-frontend/src/Chat/components/ChatBody.js
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Pusher from 'pusher-js'
 //---
 // COMPONENT_NAME: chat body 
 // INFO: all message(chat) will show here.. this is the main body section.
@@ -7,6 +7,23 @@ import React from 'react'
 import ChatMessage from './ChatMessage'
 import classes from "./ChatBody.module.css"
 const ChatBody = () => {
+    const [message, setMessage] = useState([])
+
+    useEffect(() => {
+        // this will listen for incoming message for pusher and update the message array if new message arrive. 
+        const pusher = new Pusher('8dda2a32cdc70f362461', {
+            cluster: 'ap1'
+        });
+        const message = pusher.subscribe("messages")
+        message.bind("insert", (data) => {
+            setMessage((prevState) => {
+                return [...prevState, data]
+            })
+        })
+    }, [])
+    useEffect(() => {
+        
+    }, [])
     return (
         <div className={classes.chat__body}>
             <ChatMessage />
