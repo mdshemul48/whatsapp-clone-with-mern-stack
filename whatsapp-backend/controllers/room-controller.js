341fced6f34d741205473542e3925eeb4ee91d14
MD Shimul <mdshemul480@gmail.com>
2021-06-25 17:28:28 +0600

    adding send message system

--- whatsapp-backend/controllers/room-controller.js
+++ whatsapp-backend/controllers/room-controller.js
@@ -1,14 +1,48 @@
 import Room from "../models/room.js";
+
 export const chatRoom = async (req, res) => {
-    const { id: creatorId, sender, message } = req.body;
+
+    const { id: creatorId, receiver, message } = req.body;
+
+
     let chatRoom;
     try {
-        chatRoom = await Room.find({ $and: [{ person1: creatorId }, { person2: sender }] })
+        chatRoom = await Room.findOne({ $and: [{ person1: creatorId }, { person2: receiver }] })
     } catch (err) {
         return res.status(500).send(err)
     }
 
-    console.log(chatRoom)
+
+
+    if (chatRoom) {
+        chatRoom.chat.push({
+            person: creatorId,
+            message
+        })
+        await chatRoom.save()
+        return res.status(201).send("sended.")
+    }
+
+
+
+
+
+    try {
+        const newMessage = Room({
+            person1: creatorId,
+            person2: receiver,
+            chat: [
+                {
+                    person: creatorId,
+                    message
+                }
+            ]
+        })
+        await newMessage.save()
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send("something went wrong with the server.")
+    }
 
     return res.send("hello world")
 }
--- whatsapp-backend/routes/room-routes.js
+++ whatsapp-backend/routes/room-routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
+// auth middleware 
+import authorization from "../middleware/authorization-middleware.js";
 import { chatRoom } from "../controllers/room-controller.js";
 const router = Router()
 
-router.post("/chat", chatRoom)
+router.post("/chat", authorization, chatRoom)
 export default router
