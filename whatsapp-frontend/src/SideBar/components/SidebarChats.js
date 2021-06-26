b47ef98e4e52ba3c66d526d3f52726bc79493b5e
MD Shimul <mdshemul480@gmail.com>
2021-06-27 00:09:01 +0600

    adding auth custom hooks

--- whatsapp-frontend/src/SideBar/components/SidebarChats.js
+++ whatsapp-frontend/src/SideBar/components/SidebarChats.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 
 import SidebarChat from './SidebarChat'
 // css 
 import classes from "./SidebarChats.module.css"
 const SidebarChats = () => {
+    
+    useEffect(() => {
+
+    }, [])
     return (
         <div className={classes.sidebar__chats}>
             <SidebarChat />
--- whatsapp-frontend/src/hooks/use-auth.js
+++ whatsapp-frontend/src/hooks/use-auth.js
@@ -1,5 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 const useAuth = () => {
-    const isLoggedIn = 
+    const [authInfo, setAuthInfo] = useState(undefined)
+    useEffect(() => {
+        const savedData = localStorage.getItem("auth")
+        const jsonData = JSON.parse(savedData)
+        setAuthInfo(jsonData)
+    }, [])
+    const setLoginHandler = (token, userId) => {
+        setAuthInfo({ token, userId })
+        localStorage.setItem("auth", JSON.stringify({ token, userId }))
+    }
+    const setLogoutHandler = () => {
+        localStorage.removeItem("auth")
+    }
+
+    return [authInfo, setLoginHandler, setLogoutHandler]
+
 }
+export default useAuth
