5941d699d5a7391e7dfd010727ea21d86dd1b464
MD Shimul <mdshemul480@gmail.com>
2021-06-27 00:24:26 +0600

    adding auth context and adding auth context to the index.js

--- whatsapp-frontend/src/context/authContext.js
+++ whatsapp-frontend/src/context/authContext.js
@@ -1,9 +1,21 @@
 import { createContext } from 'react'
+import useAuth from '../hooks/use-auth'
+
 
 const authContext = createContext({
     authInfo: { token: "", id: "" },
     setLoginHandler: (token, userId) => { },
     setLogoutHandler: () => { }
 })
+
+
+export const AuthContextProvider = (props) => {
+    const [authInfo, setLogInHandler, setLogoutHandler] = useAuth()
+
+    return <authContext.Provider value={{ authInfo, setLogInHandler, setLogoutHandler }}>
+        {props.children}
+    </authContext.Provider>
+}
+
 
 export default authContext
